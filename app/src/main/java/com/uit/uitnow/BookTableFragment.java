cb52821c17c2b6aa63b1d1c65b48ff523ebb0ea3
PHANPHUOCDINH <34784956+PHANPHUOCDINH@users.noreply.github.com>
2019-11-01 04:25:49 +0700

    update màn hình BookTable & OrderFood

--- app/src/main/java/com/uit/uitnow/BookTableFragment.java
+++ app/src/main/java/com/uit/uitnow/BookTableFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,12 +22,17 @@ public class BookTableFragment extends Fragment {
     RestaurantAdapter resAdapter;
     ArrayList<Restaurant> listRests;
     SwipeRefreshLayout swipeRests;
+    AppCompatSpinner spinner;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.booktable_fragment, container, false);
         rvRests = view.findViewById(R.id.rvRestaurants);
         swipeRests=view.findViewById(R.id.swipeRestaurants);
+        spinner=view.findViewById(R.id.spinnerDistrict);
+        String[] districts={"Quận 1","Quận 2","Quận 3","Quận 4","Quận 5"};
+        ArrayAdapter<String> adapterDistrict=new ArrayAdapter<>(getActivity(),R.layout.support_simple_spinner_dropdown_item,districts);
+        spinner.setAdapter(adapterDistrict);
         swipeRests.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -39,9 +46,10 @@ public class BookTableFragment extends Fragment {
     private void getRestaurants()
     {
         // kết nối firebase lấy dữ liệu
-        resAdapter = new RestaurantAdapter(listRests);
-        rvRests.setAdapter(resAdapter);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        rvRests.setLayoutManager(layoutManager);
+
+//        resAdapter = new RestaurantAdapter(listRests);
+//        rvRests.setAdapter(resAdapter);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+//        rvRests.setLayoutManager(layoutManager);
     }
 }
--- app/src/main/java/com/uit/uitnow/OrderFoodFragment.java
+++ app/src/main/java/com/uit/uitnow/OrderFoodFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,6 +23,7 @@ public class OrderFoodFragment extends Fragment {
     ArrayList<Store> listStores;
     SwipeRefreshLayout swipeStores;
     TextView tvMyAddress;
+    AppCompatSpinner spinner;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,6 +31,10 @@ public class OrderFoodFragment extends Fragment {
         tvMyAddress=view.findViewById(R.id.tvMyAddress);
         rvStores = view.findViewById(R.id.rvStores);
         swipeStores=view.findViewById(R.id.swipeStores);
+        spinner=view.findViewById(R.id.spinnerDistrict);
+        String[] districts={"Quận 1","Quận 2","Quận 3","Quận 4","Quận 5"};
+        ArrayAdapter<String> adapterDistrict=new ArrayAdapter<>(getActivity(),R.layout.support_simple_spinner_dropdown_item,districts);
+        spinner.setAdapter(adapterDistrict);
         swipeStores.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -41,10 +48,11 @@ public class OrderFoodFragment extends Fragment {
     private void getStores()
     {
         // kết nối firebase lấy dữ liệu
-        storeAdapter = new StoreAdapter(listStores);
-        rvStores.setAdapter(storeAdapter);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        rvStores.setLayoutManager(layoutManager);
+
+//        storeAdapter = new StoreAdapter(listStores);
+//        rvStores.setAdapter(storeAdapter);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+//        rvStores.setLayoutManager(layoutManager);
     }
 
 }
